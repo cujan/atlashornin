c40890c357b97981ceef11bdcd6b21938cbfba73
holubjan <holubjan@gmail.com>
2014-02-04 18:22:00 +0000

    dorobene ciselniky

--- app/model/CiselnikSkupinaRepository.php
+++ app/model/CiselnikSkupinaRepository.php
@@ -4,5 +4,8 @@ namespace Todo;
 use Nette;
 
 class CiselnikSkupinaRepository extends Repository {
-    
+     public function findById($id)
+    {
+	return $this->findAll()->get($id);
+    }
 }
--- app/model/LokalitaSlovenskoRepository.php
+++ app/model/LokalitaSlovenskoRepository.php
@@ -4,5 +4,8 @@ namespace Todo;
 use Nette;
 
 class LokalitaSlovenskoRepository extends Repository {
-    
+     public function findById($id)
+    {
+	return $this->findAll()->get($id);
+    }
 }
